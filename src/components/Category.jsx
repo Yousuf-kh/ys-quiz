3ef79d5a0ee64eeac358f12fd45aa59291adb37d
Yusuf <yusuf2002kh@gmail.com>
2025-01-21 18:43:04 +0300

    edit:userName

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -9,7 +9,7 @@ const Category = () => {
   return (
     <Box align={'center'} bg={'blue.800'} minH={'100dvh'}>
       <Container maxW={'container.md'}>
-        <Heading textAlign={'center'} color={"#fff"} p={5}>Выберайте тему {userName ? userName : 'User'}</Heading>
+        <Heading textAlign={'center'} color={"#fff"} p={5}>Выберайте тему <Box color={'red.500'} as='span'>{userName ? userName : 'User'}</Box></Heading>
         <Flex h={'70dvh'} justify={'center'} flexDir={'column'} gap={5} maxW={{md:'60%',base:''}} mx={'auto'}>
           <Flex bg={'green.500'} px={5} py={3} borderRadius={'20px'} color={'white'} align={'center'} justify={'center'} gap={6}>
             <Image w={{md:'120px',base:'80px'}} src={'/icon1.png'} />
